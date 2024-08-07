cfda27918b2155736dc09524746f932d4517ce54
Andrei <126566419+Xeotheosis@users.noreply.github.com>
2024-08-06 18:16:01 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { React } from "react";
+import Header from "./Header";
+import MainMenu from "./MainMenu";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <MainMenu />
+    </>
+  );
+}
+
+export default App;
